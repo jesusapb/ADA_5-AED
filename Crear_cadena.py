be86f4faa55f52745c74216f56bb832dcbd65cb4
Unknown <jesusapb29@gmail.com>
2021-04-27 00:40:14 -0500

    creacion del proyecto

---
+++ Crear_cadena.py
@@ -0,0 +1,18 @@
+import random
+
+class Crear_cadena:
+
+    def __init__(self,tama):
+        self.tama = tama
+        self.cadena = []
+
+
+    def numero_aleatorio(self):
+        pass
+
+
+    def construir(self):
+        pass
+
+    def imprimir_cadena(self):
+        pass
